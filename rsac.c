28341b59783797421e1404cc67b02d49cebdcab1
EstevanBR <estevan@bluerocket.us>
2016-03-01 01:28:03 -0800

    incorporating rsac work

--- rsac.c
+++ rsac.c
@@ -1,3 +1,4 @@
+#include "rsa.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -6,17 +7,6 @@
 #include <ctype.h>
 #include <limits.h>
 #define BLURB "\n\nCRYPT:\n\tlate Middle English\n\t(in the sense ‘cavern’):\n\tfrom Latin crypta,\n\tfrom Greek kruptē\n\t‘a vault,’ from kruptos ‘hidden.’\n\n\n"
-
-void encryptToFile (char const *textToProcess, unsigned long const p, unsigned long const q, const char *fileName);
-void decryptFromFileToFile(unsigned long const p, unsigned long const q, const char *fileName, const char *fileNameDest);
-unsigned long modpow(unsigned long base,unsigned long exponent,unsigned long modulus);
-void clear(void);
-unsigned long modInverse (unsigned long, unsigned long);
-unsigned long gcd (unsigned long, unsigned long);
-unsigned long gcdExtended (unsigned long, unsigned long, unsigned long*, unsigned long*);
-char *inputString (FILE* fp, size_t size, char terminatingCharacter);
-unsigned long getUnsignedLongFromStream(FILE *fp);
-int validateN(unsigned long n);
 
 int main(int argc, char const *argv[]) {
 	for (int i = 1; i < argc; i++) {
